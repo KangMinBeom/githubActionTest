034de7ecfb9362c14c659c7ba6c35e5d0d034655
kangminbeom <alsqja6945@naver.com>
2023-12-02 21:43:57 +0900

    githubaction 배포

--- src/main/java/com/example/githubaction/controller/HomeController.java
+++ src/main/java/com/example/githubaction/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.githubaction.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	@Value("${app.version:1.0.0}")
+	String version;
+
 	@GetMapping("/")
 	public String home(){
-		return "index.html";
+		return "Hello CICD! version:" + version;
 	}
 }
